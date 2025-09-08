d9fdc90d650be890ef406ff4815fbc36ab03419d
faa6illinois <faa6@illinois.edu>
2025-09-07 23:32:19 -0400

    Create FA Calculation.py

---
+++ FA Calculation.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+#Take user input in specified format
+date_of_birth = input("Enter your date of birth in '(MM/DD/YYYY)' format:")
+
+
+#Convert DOB to date
+convert_date = datetime.strptime(date_of_birth, "%m/%d/%Y").date()
+
+
+today = date.today()
+
+
+calculate_days = today - convert_date
+
+print(f"You are: {calculate_days} days old!")
